11d948bf8f5dcc367c5cc560adf422c846c18820
Mauricio Miranda <maurimiranda@gmail.com>
2016-10-19 20:40:16 -0300

    Add search control. Replace atm data by schools. Add mapproxy configuration. Get stamen through mapproxy.

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,31 @@
 L.Icon.Default.imagePath = 'images/';
 
-/* Crear mapa de Leaflet */
+// Crea mapa de Leaflet
 var map = L.map('map', {
   center: [23.1341, -82.3617],
-  zoom: 14
+  zoom: 15
 });
 
-/* Agregar capa base de stamen */
-new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+// Agrega capa base de Stamen
+// new L.tileLayer('http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.png', {
+// minZoom: 0,
+// maxZoom: 19,
+// attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+// }).addTo(map);
+
+// Agrega capa base de Stamen via MapProxy
+var baseLayer = new L.tileLayer('http://localhost:8080/tiles/1.0.0/stamen.watercolor/WGS84/{z}/{x}/{y}.png', {
   minZoom: 0,
-  maxZoom: 18,
+  maxZoom: 17,
   attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
 }).addTo(map);
 
-/* Agregar capa con datos de GeoJSON */
-L.geoJson(atm, {
+// Agrega capa con datos de GeoJSON
+var schoolsLayer = L.geoJson(schools, {
   pointToLayer: function(feature, latlng) {
     return L.circleMarker(latlng, {
       radius: 10,
-      color: '#FFF',
+      color: '#000',
       weight: 3,
       opacity: 0.5,
       fillOpacity: 0.9,
@@ -26,11 +33,49 @@ L.geoJson(atm, {
     });
   },
   onEachFeature: function(feature, layer) {
-    var text = feature.properties.name ? feature.properties.name : 'Cajero sin nombre';
+    var text = feature.properties.name ? feature.properties.name : 'Escuela sin nombre';
     layer.bindPopup(text);
   }
 }).addTo(map);
 
+// Initializa buscador
+var fuse = new Fuse(schools.features, {
+  keys: ['properties.name']
+});
+
+// Agrega caja de búsqueda
+var searchControl = new L.Control.Search({
+  layer: schoolsLayer,
+  propertyName: 'name',
+  circleLocation: false,
+  moveToLocation: function(latlng, title, map) {
+    var zoom = map.getBoundsZoom(latlng.layer.getBounds());
+    map.setView(latlng, zoom);
+  },
+  textPlaceholder: 'Buscar...',
+  textErr: 'No hay resultados.',
+  textCancel: 'Cancelar',
+  filterData: function(text, records) {
+    var filter = {};
+    fuse.search(text).forEach(function(result) {
+      filter[result.properties.name] = records[result.properties.name];
+    });
+    return filter;
+  }
+});
+
+searchControl.on('search:locationfound', function(e) {
+  e.layer.setStyle({radius: 20});
+  e.layer.openPopup();
+}).on('search:collapsed', function(e) {
+  schoolsLayer.eachLayer(function(layer) {  //restore feature color
+    layer.setStyle({radius: 10});
+  });
+});
+
+searchControl.addTo(map);
+
+// Genera colores aleatorios
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
---
+++ scripts/schools.js
@@ -0,0 +1,779 @@
+var schools = {
+  "type": "FeatureCollection",
+  "generator": "overpass-turbo",
+  "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+  "timestamp": "2016-10-19T18:52:03Z",
+  "features": [
+    {
+      "type": "Feature",
+      "id": "node/343275875",
+      "properties": {
+        "@id": "node/343275875",
+        "amenity": "school",
+        "name": "Joven Club de Computación y Electrónica"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.353339,
+          23.135627
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/429272410",
+      "properties": {
+        "@id": "node/429272410",
+        "amenity": "school",
+        "name": "Escuela Primaria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3554062,
+          23.1381268
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/429272411",
+      "properties": {
+        "@id": "node/429272411",
+        "amenity": "school",
+        "name": "Escuela Primaria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3555367,
+          23.1379624
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/862176657",
+      "properties": {
+        "@id": "node/862176657",
+        "amenity": "school",
+        "name": "Anton Makarenko"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4171039,
+          23.1267003
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1037074080",
+      "properties": {
+        "@id": "node/1037074080",
+        "amenity": "school",
+        "name": "Cátedra Humboldt"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3812124,
+          23.1354418
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1315119988",
+      "properties": {
+        "@id": "node/1315119988",
+        "addr:street": "Tulipán",
+        "amenity": "school",
+        "name": "Combatientes de Bolivia",
+        "name:es": "Combatientes de Bolivia"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3963662,
+          23.1166885
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1315120000",
+      "properties": {
+        "@id": "node/1315120000",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3922737,
+          23.1226171
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1315120025",
+      "properties": {
+        "@id": "node/1315120025",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3982652,
+          23.1140242
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/1317835298",
+      "properties": {
+        "@id": "node/1317835298",
+        "addr:housename": "San Martin",
+        "addr:housenumber": "1171",
+        "amenity": "school",
+        "name": "Casa MAX"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3791074,
+          23.1347813
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2621617121",
+      "properties": {
+        "@id": "node/2621617121",
+        "addr:city": "La Habana",
+        "addr:housenumber": "104",
+        "addr:postcode": "10400",
+        "addr:street": "Calzada (7ma)",
+        "amenity": "school",
+        "name": "Escuela de Automovilismo",
+        "operator": "Cubataxi"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3923948,
+          23.1439141
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2622674582",
+      "properties": {
+        "@id": "node/2622674582",
+        "amenity": "school",
+        "name": "Republica de Panama Primaria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3704762,
+          23.105635
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2652295875",
+      "properties": {
+        "@id": "node/2652295875",
+        "amenity": "school",
+        "name": "Alianza Francesa",
+        "operator": "Embajada de Francia en Cuba"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3897158,
+          23.1387085
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2652295880",
+      "properties": {
+        "@id": "node/2652295880",
+        "amenity": "school",
+        "name": "Alianza Francesa edificio anexo",
+        "operator": "Embajada de Francia"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3882502,
+          23.1416189
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2837436112",
+      "properties": {
+        "@id": "node/2837436112",
+        "amenity": "school",
+        "building": "school",
+        "name": "Escuela Emilio Nuñes Tallapiedra",
+        "operator": "Mined"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3612258,
+          23.1241082
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/2946368538",
+      "properties": {
+        "@id": "node/2946368538",
+        "amenity": "school",
+        "name": "IPU Tomás David Royo",
+        "operator": "Mined"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3912804,
+          23.1391684
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3203725180",
+      "properties": {
+        "@id": "node/3203725180",
+        "amenity": "school",
+        "name": "Conservatorio Amadeo Roldan"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3672475,
+          23.1265973
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/3935354716",
+      "properties": {
+        "@id": "node/3935354716",
+        "addr:city": "Vedado, Havanna",
+        "addr:postcode": "10400",
+        "addr:street": "Calle L",
+        "amenity": "school",
+        "name": "Ballet School",
+        "operator": "Ballet Nacional de Cuba"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3847878,
+          23.1414857
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4145587789",
+      "properties": {
+        "@id": "node/4145587789",
+        "amenity": "school",
+        "name": "Mártires del Segundo Frente"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4039154,
+          23.1122827
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4145587989",
+      "properties": {
+        "@id": "node/4145587989",
+        "addr:street": "28",
+        "amenity": "school",
+        "name": "Escuela Secundaria Humber d Blanc"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4025224,
+          23.1168378
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4151867495",
+      "properties": {
+        "@id": "node/4151867495",
+        "addr:street": "Carvajal",
+        "amenity": "school",
+        "name": "Braulio Coroneaux",
+        "opening_hours": "Mo-Fr 08:00-17:00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3755897,
+          23.1109865
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4154378909",
+      "properties": {
+        "@id": "node/4154378909",
+        "addr:street": "45",
+        "amenity": "school",
+        "name": "Escuela Especial Ernestito"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4102431,
+          23.1157816
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4232500319",
+      "properties": {
+        "@id": "node/4232500319",
+        "amenity": "school",
+        "name": "Instituto Superior de Relaciones Internacionales"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3929171,
+          23.1439056
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4243440992",
+      "properties": {
+        "@id": "node/4243440992",
+        "amenity": "school",
+        "name": "conjunto folklorico nacional de cuba"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3953659,
+          23.1289868
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4253558089",
+      "properties": {
+        "@id": "node/4253558089",
+        "addr:street": "14",
+        "amenity": "school",
+        "name": "CEEH",
+        "opening_hours": "Mo-Fr 08:00-17:30"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4172936,
+          23.1232885
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4285034395",
+      "properties": {
+        "@id": "node/4285034395",
+        "addr:street": "A",
+        "amenity": "school",
+        "name": "Centro Preuniversitario de Economía y Gastronomía Antonio Guiteras Holmes",
+        "opening_hours": "Mo-Fr 08:00-06:00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3931784,
+          23.1316641
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4285036489",
+      "properties": {
+        "@id": "node/4285036489",
+        "amenity": "school",
+        "name": "Facultad de Comunicación Social"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3870135,
+          23.1180485
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4293609993",
+      "properties": {
+        "@id": "node/4293609993",
+        "amenity": "school",
+        "name": "школьная форма"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3542006,
+          23.1344195
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4298533902",
+      "properties": {
+        "@id": "node/4298533902",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3706213,
+          23.1315257
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4298653302",
+      "properties": {
+        "@id": "node/4298653302",
+        "addr:street": "San Ignacio",
+        "amenity": "school",
+        "name": "Vitrina de Valonia"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3504654,
+          23.1360994
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4298667597",
+      "properties": {
+        "@id": "node/4298667597",
+        "addr:street": "San Ignacio",
+        "amenity": "school",
+        "building": "school",
+        "name": "Escuela Primaria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3505095,
+          23.1369697
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4311155588",
+      "properties": {
+        "@id": "node/4311155588",
+        "amenity": "school",
+        "name": "Escuela Primaria Manuel Ascunce"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3620422,
+          23.1304608
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4311165476",
+      "properties": {
+        "@id": "node/4311165476",
+        "addr:street": "Calzada del Cerro y Monasterio",
+        "amenity": "school",
+        "name": "ESBU José Martí"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3869158,
+          23.1086491
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4311169517",
+      "properties": {
+        "@id": "node/4311169517",
+        "amenity": "school",
+        "name": "Escuela Primaria Elpidio Sosa"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3787934,
+          23.1220595
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4341059665",
+      "properties": {
+        "@id": "node/4341059665",
+        "amenity": "school",
+        "name": "Escuela Secundaria Básica"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4240495,
+          23.1117799
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4341059892",
+      "properties": {
+        "@id": "node/4341059892",
+        "amenity": "school",
+        "name": "Escuela Secundaria Básica"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.4239412,
+          23.1120437
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4341059901",
+      "properties": {
+        "@id": "node/4341059901",
+        "amenity": "school",
+        "name": "Escuela de Educadores \"Vilma Espín Guillois\""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3511081,
+          23.1004531
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4358616891",
+      "properties": {
+        "@id": "node/4358616891",
+        "addr:postcode": "10100",
+        "addr:street": "Brasil (Teniente Rey)",
+        "amenity": "school",
+        "name": "Escuela Primaria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3529095,
+          23.1360665
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4358649298",
+      "properties": {
+        "@id": "node/4358649298",
+        "addr:postcode": "10100",
+        "addr:street": "Avenida de Bélgica (Egido)",
+        "amenity": "school",
+        "building": "school",
+        "name": "Escuela Primaria",
+        "opening_hours": "Mo-Fr 07:00-17:00"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3554172,
+          23.1320005
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4358677589",
+      "properties": {
+        "@id": "node/4358677589",
+        "addr:street": "Máximo Gómez (Monte)",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3598948,
+          23.1317528
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4368127192",
+      "properties": {
+        "@id": "node/4368127192",
+        "addr:street": "Brasil (Teniente Rey)",
+        "amenity": "school",
+        "name": "Escuela Primaria"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3501816,
+          23.1364503
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4385401312",
+      "properties": {
+        "@id": "node/4385401312",
+        "addr:street": "Agramonte (Zulueta)",
+        "amenity": "school",
+        "name": "Instituto Preuniversitario José Martí"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3577921,
+          23.1361499
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4418202991",
+      "properties": {
+        "@id": "node/4418202991",
+        "addr:street": "San Miguel",
+        "amenity": "school",
+        "name": "Britannia de Inglea",
+        "name:es": "Britannia de Inglea"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3695183,
+          23.1368594
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4418679549",
+      "properties": {
+        "@id": "node/4418679549",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3572119,
+          23.1071377
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4418679550",
+      "properties": {
+        "@id": "node/4418679550",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.355085,
+          23.1091711
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4422164219",
+      "properties": {
+        "@id": "node/4422164219",
+        "amenity": "school"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3543959,
+          23.108844
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "id": "node/4428325990",
+      "properties": {
+        "@id": "node/4428325990",
+        "addr:street": "Conill",
+        "amenity": "school",
+        "name": "IPU José Miguel Pérez"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.3938598,
+          23.1145495
+        ]
+      }
+    }
+  ]
+}
